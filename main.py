6d1fa23633da7d89f89e442ba24a836857d63fa0
Hamis <khamismitch@gmail.com>
2024-11-24 09:05:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 # soup = BeautifulSoup(html_content, 'html.parser')
 
-
+# define sports data api
 sports_apis = [
     {
         "sport": "Football",
@@ -55,10 +55,10 @@ sports_apis = [
     },
 ]
 
-
+#retrieve game data
 def fetch_games(api_url, params, sport_name):
     games = []
-    while True:
+    while True: # run it continously stop when there is an error
         response = requests.get(api_url, params=params)
         if response.status_code != 200:
             print(f"Failed to fetch data for {sport_name}: {response.status_code}")
@@ -67,7 +67,7 @@ def fetch_games(api_url, params, sport_name):
         data = response.json()
         matches = data.get("data", [])
 
-        if not matches:
+        if not matches: #stop if no matches are available
             break
 
         for match in matches:
@@ -81,27 +81,30 @@ def fetch_games(api_url, params, sport_name):
                     match.get("category", "Unknown"),
                 ]
             )
-
+        #check if there is another page to fetch
         if not data.get("pagination", {}).get("has_next_page", False):
             break
         params["page"] += 1
 
     return games
 
-
+#store all sports data
 all_games = []
 
+#loop throush sports data and store them
 for sport in sports_apis:
     print(f"Fetching data for {sport['sport']}...")
     sport_games = fetch_games(sport["url"], sport["params"], sport["sport"])
     all_games.extend(sport_games)
 
-
+#store the data in csv
 with open("betika_all_games.csv", "w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
+    #add columns
     writer.writerow(
         ["Sport", "Home Team", "Away Team", "Start Time", "Competition", "Category"]
     )
+    #add rows
     writer.writerows(all_games)
 
-# print("Data saved to betika_all_games.csv")
+
